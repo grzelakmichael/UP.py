9a552f626f1c02124bdc5e7bab6bf48cb73bbdce
Michal Grzelak <michal.grzelak@stxnext.pl>
2018-05-22 15:01:41 +0200

    spotkanie 5

--- Spotkanie5/46ComputingBodyMassIndex.py
+++ Spotkanie5/46ComputingBodyMassIndex.py
@@ -4,8 +4,8 @@ weight = eval(input("Enter weight in pounds: "))
 # Prompt the user to enter height in inches
 height = eval(input("Enter height in inches: "))
 
-KILOGRAMS_PER_POUND = 0.45359237 # Constant
-METERS_PER_INCH = 0.0254 # Constant
+KILOGRAMS_PER_POUND = 0.45359237  # Constant
+METERS_PER_INCH = 0.0254  # Constant
 
 # Compute BMI
 weightInKilograms = weight * KILOGRAMS_PER_POUND
